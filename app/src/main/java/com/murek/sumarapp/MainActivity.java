cc156c869a45771606e4118cc4ae4ec0079a7245
Nicolas Kaminski <murek28@gmail.com>
2025-02-10 15:28:26 -0300

    update a binding

--- app/src/main/java/com/murek/sumarapp/MainActivity.java
+++ app/src/main/java/com/murek/sumarapp/MainActivity.java
@@ -12,31 +12,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.murek.sumarapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-    private EditText et1;
-    private EditText et2;
-    private TextView tvResult;
-    private Button btSum;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et1 = findViewById(R.id.etNumero1);
-        et2 = findViewById(R.id.etNumero2);
-        tvResult = findViewById(R.id.tvResultado);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-    }
+        binding.btsuma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sumar(view);
+            }
+        });
+        }
+
 
     public void sumar(View view) {
-        String num1 = et1.getText().toString();
-        String num2 = et2.getText().toString();
+        String num1 = binding.etNumero1.getText().toString();
+        String num2 = binding.etNumero2.getText().toString();
 
         int n1 = Integer.parseInt(num1);
         int n2 = Integer.parseInt(num2);
         int res = n1 + n2;
-        tvResult.setText("El resultado es: " + res);
+        binding.tvResultado.setText("El resultado es: " + res);
     }
 }
